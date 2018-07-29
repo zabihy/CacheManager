bb02e1fac8c5d41dc582137f476c10ed46539da2
morteza zabihi <mz2019@gmail.com>
2018-07-30 02:00:09 +0430

    CustomerStore added

---
+++ src/Sample.DataAccess/CustomerStore.cs
@@ -0,0 +1,21 @@
+﻿using Sample.Entities.Customers;
+using System;
+using System.Collections.Generic;
+
+namespace Sample.DataAccess
+{
+    public class CustomerStore:List<Customer>
+    {
+        public CustomerStore()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                this.Add(new Customer
+                {
+                    Id = i,
+                    Name = $"Customer_{i}"
+                });
+            }
+        }
+    }
+}
