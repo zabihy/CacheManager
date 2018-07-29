70828121d18d940e948444cebc2c5cf8e9265de3
morteza <mz2019@gmail.com>
2018-07-29 11:30:12 +0430

    Initialize

---
+++ XCache/Contracts/IEntityForCaching.cs
@@ -0,0 +1,9 @@
+namespace XCache
+{
+    /// <summary>
+    /// Cacheable entity (EF doesn't support 2-level caching now)
+    /// </summary>
+    public interface IEntityForCaching
+    {
+    }
+}
